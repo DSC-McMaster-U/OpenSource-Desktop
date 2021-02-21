936f507a407a7607600fa99d7304f29550004c29
amujtaba00 <mujtaa3@mcmaster.ca>
2021-02-20 23:01:45 -0500

    Added Color

--- src/managementTab/ledger/Ledger.js
+++ src/managementTab/ledger/Ledger.js
@@ -1,42 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
-import LedgerItem from './LedgerItem';
-import financesData from './dummyData'
-import { FixedSizeList } from 'react-window';
-import IconButton from '@material-ui/core/IconButton';
-import AddIcon from '@material-ui/icons/Add';
-import Divider from '@material-ui/core/Divider';
-import Form from './form'
-import Button from '@material-ui/core/Button';
-
-
-
-
-
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Collapse from "@material-ui/core/Collapse";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import SendIcon from "@material-ui/icons/Send";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import StarBorder from "@material-ui/icons/StarBorder";
+import LedgerItem from "./LedgerItem";
+import financesData from "./dummyData";
+import { FixedSizeList } from "react-window";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
+import Divider from "@material-ui/core/Divider";
+import Form from "./form";
+import Button from "@material-ui/core/Button";
 
 // Style Variable
-const style={
-  display: 'flex',
-    textAlign: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    border: '1px solid black',
-    paddingLeft: '20px'
-}
-
+const style = {
+  display: "flex",
+  textAlign: "center",
+  justifyContent: "center",
+  alignItems: "center",
+  border: "1px solid black",
+  paddingLeft: "20px",
+};
 
 // Props for Form
 Form.propTypes = {
@@ -45,55 +39,57 @@ Form.propTypes = {
   selectedValue: PropTypes.string.isRequired,
 };
 
-
 // Main ledger component
-function Ledger(){
-
+function Ledger() {
   // Row variables to construct LedgerItem components
-  var Row = financesData.map( (item, index) =>{ 
-    return <div>
-           <LedgerItem items={financesData[index]}/>
-       <Divider variant="middle" />
-       </div>
-  })
+  var Row = financesData.map((item, index) => {
+    return (
+      <div>
+        <LedgerItem items={financesData[index]} />
+        <Divider variant="middle" />
+      </div>
+    );
+  });
 
-  // State variables for open and closing form 
+  // State variables for open and closing form
   const [open, setOpen] = React.useState(false);
   const [selectedValue, setSelectedValue] = React.useState("");
-  
+
   // Function to open form
   const handleClickOpen = () => {
     setOpen(true);
   };
-  
+
   // Function to close form
   const handleClose = (value) => {
     setOpen(false);
     setSelectedValue(value);
   };
 
-  
-
   // Return value for Ledger Component
   return (
-
     // Encompassing Div
-      <div style={{textAlign: 'center'}}>
+    <div style={{ textAlign: "center" }}>
       {/* Material-UI List component start */}
       <List
         className="ledger"
         title="Ledger"
         style={{
-            width:'300px',
-            margin:'auto'
+          width: "300px",
+          margin: "auto",
+          backgroundColor: "#850044",
         }}
       >
         {/* Ledger Title */}
-        <h1 style={{fontSize:'30px',textAlign: 'center'}}>Ledger</h1>
+        <h1 style={{ fontSize: "30px", textAlign: "center", color: "#f3c761" }}>
+          Ledger
+        </h1>
         {/* LedgerItems Components */}
         {Row}
         {/* Button that opens form */}
-        <Button onClick={handleClickOpen}>Add Item  <AddIcon /> </Button>
+        <Button onClick={handleClickOpen} style={{ color: "#F3C761" }}>
+          Add Item <AddIcon />{" "}
+        </Button>
       </List>
       <br></br>
 
@@ -105,9 +101,7 @@ function Ledger(){
       <br></br>
       <br></br>
       <br></br>
-
-      </div>
-    )
-  
+    </div>
+  );
 }
 export default Ledger;
--- src/managementTab/ledger/LedgerItem.js
+++ src/managementTab/ledger/LedgerItem.js
@@ -1,107 +1,108 @@
-import React from 'react';
+import React from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Collapse from '@material-ui/core/Collapse';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import './ledgerStyle.css'
-
-
+import { makeStyles } from "@material-ui/core/styles";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Collapse from "@material-ui/core/Collapse";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import "./ledgerStyle.css";
 
 // Style variable for List Item Components
 const useStyles = makeStyles({
-    root: {
-      minWidth: 275,
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-  });
-
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 // List Item Component
-function LedgerItem(props){ 
-  
+function LedgerItem(props) {
   const classes = useStyles();
 
   // State variables for List Item
-  const [checked,setChecked]= React.useState(false);
+  const [checked, setChecked] = React.useState(false);
   const [open, setOpen] = React.useState(false);
 
   // Function for handling the expanding of Item Description Card
   const handleClick = () => {
-      setOpen(!open);
-    };
+    setOpen(!open);
+  };
 
+  // Function for handling the checkbox
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+    var mainDivStyle =
+      event.target.parentNode.parentElement.parentElement.parentElement
+        .parentElement.style;
+    mainDivStyle.display = "none";
+  };
 
-
-    // Function for handling the checkbox
-    const handleChange = (event) =>{
-        setChecked(event.target.checked);
-        var mainDivStyle = event.target.parentNode.parentElement.parentElement.parentElement.parentElement.style
-        mainDivStyle.display = "none";
-    }
-
-
-    // Return value for main Ledger Item Component
-    return(
-
-      // Encompassing Div
-      <div>
-    {/* Main list item component */}
+  // Return value for main Ledger Item Component
+  return (
+    // Encompassing Div
+    <div>
+      {/* Main list item component */}
       <ListItem>
-      <ListItemText primary={props.items.name} button onClick={handleClick} />
-      {open ? <ExpandLess button onClick={handleClick}/> : <ExpandMore button onClick={handleClick}/>}
-      <FormControlLabel
-      control={
-        <Checkbox
-          checked={checked}
-          onChange={handleChange}
-          name="checkedB"
-          color="primary"
+        <ListItemText primary={props.items.name} button onClick={handleClick} />
+        {open ? (
+          <ExpandLess button onClick={handleClick} />
+        ) : (
+          <ExpandMore button onClick={handleClick} />
+        )}
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={checked}
+              onChange={handleChange}
+              name="checkedB"
+              color="primary"
+            />
+          }
+          labelPlacement="start"
         />
-      }
-      labelPlacement="start"
-    />
-    </ListItem>
-    {/* Collapsible Card for ledger item description */}
-    <Collapse in={open} timeout="auto" unmountOnExit>
-      <Card className={classes.root} variant="outlined">
-      <CardContent>
-          <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Due on {props.items.dueDate}
-          </Typography>
-          <Typography variant="h5" component="h2">
-          {props.items.name}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
+      </ListItem>
+      {/* Collapsible Card for ledger item description */}
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <Card className={classes.root} variant="outlined">
+          <CardContent>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+            >
+              Due on {props.items.dueDate}
+            </Typography>
+            <Typography variant="h5" component="h2">
+              {props.items.name}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
               CAD ${props.items.amount}
-          </Typography>
-          <Typography variant="body2" component="p">
-          {props.items.description}
-          </Typography>
-      </CardContent>
-      
-      </Card>
-    </Collapse>
+            </Typography>
+            <Typography variant="body2" component="p">
+              {props.items.description}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Collapse>
     </div>
-    )
+  );
 }
 
-export default LedgerItem
+export default LedgerItem;
